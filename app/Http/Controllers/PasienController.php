d51652e78dc521126e217b32d8dd14636431aecd
Achmad Farhan M <farhanjuve@gmail.com>
2020-11-15 20:20:27 +0700

    + carbon
    carbon on pasiencontroller

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pasien;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
 
 class PasienController extends Controller
 {
